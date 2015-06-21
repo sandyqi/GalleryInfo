23739f7fed233b84e7cf0e49b346ab6f780f5cf7
Sandy <qm0920@hotmail.com>
2015-06-21 10:30:38 -0700

    first update
    complete angular webpage

---
+++ GalleryInfo/js/app.js
@@ -0,0 +1,21 @@
+﻿var myApp = angular.module('myApp', [
+        'ngRoute',
+        'galleryControllers'
+]);
+
+myApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/gallery', {
+                templateUrl: 'partial/galleryList.html',
+                controller: 'listController'
+            }).
+            when('/gallery/:galleryId', {
+                templateUrl: 'partial/galleryDetail.html',
+                controller: 'detailController'
+            }).
+            otherwise({
+                redirectTo: '/gallery'
+            });
+
+}]);
---
+++ GalleryInfo/js/controller.js
@@ -0,0 +1,17 @@
+﻿var galleryControllers = angular.module('galleryControllers', []);
+
+galleryControllers.controller('listController', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('data/galleriesInfo.html').success(function (data) {
+            $scope.galleries = data;
+        })
+        $scope.ordered = 'name';
+    }]);
+
+galleryControllers.controller('detailController', ['$scope', '$routeParams','$http',
+    function ($scope, $routeParams, $http) {
+        $scope.galleryId = $routeParams.galleryId;
+        $http.get('data/'+$routeParams.galleryId+'.html').success(function (data) {
+            $scope.gallery = data;
+        });
+    }]);
